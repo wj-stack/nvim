9a268d8c298d80e27c9128464992bc248c85c22b
JIE.WU <JIE.WU@DELTAWW.COM>
2022-10-16 07:11:46 -0700

    add lsp attach

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ sudo apt update
 sudo apt install nodejs npm
 
 
+cargo install stylua
+go install mvdan.cc/gofumpt@latest
+
 目前nvim 7.2
+
+
+mapbuf("n", "<leader>f", "<cmd>lua vim.lsp.buf.format { auync = true } <CR>", opt)
--- lua/lsp/config/lua.lua
+++ lua/lsp/config/lua.lua
@@ -30,20 +30,7 @@ local opts = {
 	flags = {
 		debounce_text_changes = 150,
 	},
-	on_attach = function(client, bufnr)
-		-- 禁用格式化功能，交给专门插件插件处理
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
-
-		local function buf_set_keymap(...)
-			vim.api.nvim_buf_set_keymap(bufnr, ...)
-		end
-
-		-- 绑定快捷键
-		require("keybindings").mapLSP(buf_set_keymap)
-		-- 保存时自动格式化
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-	end,
+	on_attach = require("lsp.attach"),
 }
 
 -- 查看目录等信息
